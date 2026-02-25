3a9fe768bf4b8152f5efb0431dced598fd7fe295
Timoteo <timoteo@hdinfo-ma.com>
2026-02-25 19:53:37 -0300

    feat: Implementação da interface do tools model para repository.

---
+++ src/models/interfaces/tools_interface.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+
+class InterfaceTools(ABC):
+    
+    @abstractmethod
+    def list_tools(self) -> list: ...
+    
+    @abstractmethod
+    def get_tools_for_tags(self) -> list: ...
+    
+    @abstractmethod
+    def create_tools(self) -> None: ...
+    
+    @abstractmethod
+    def delete_tools(self) -> None: ...
