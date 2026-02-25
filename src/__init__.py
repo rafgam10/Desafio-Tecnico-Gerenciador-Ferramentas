39d6cf2177bbd8e6bba6460992f0a7adab3338e0
Timoteo <timoteo@hdinfo-ma.com>
2026-02-25 19:41:53 -0300

    config: configuração do JWT no ambiente Flask.

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from .settings.config import Config
-from .settings.extensions import db, migrate
+from .settings.extensions import db, migrate, jwt
 
 def create_app():
     app = Flask(__name__)
@@ -8,6 +8,7 @@ def create_app():
 
     db.init_app(app)
     migrate.init_app(app, db)
+    jwt.init_app(app)
 
     try:
         from .routes import register_routes
