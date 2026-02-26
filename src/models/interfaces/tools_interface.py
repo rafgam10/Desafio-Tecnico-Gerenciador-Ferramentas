fbcc436237addfc3685d083577331cf3172aceda
Timoteo <timoteo@hdinfo-ma.com>
2026-02-26 14:46:01 -0300

    feat: Correção de formatação.

--- src/models/interfaces/tools_interface.py
+++ src/models/interfaces/tools_interface.py
@@ -1,15 +1,19 @@
 from abc import ABC, abstractmethod
 
+
 class InterfaceTools(ABC):
-    
     @abstractmethod
-    def list_tools(self) -> list: ...
-    
+    def list_tools(self) -> list:
+        ...
+
     @abstractmethod
-    def get_tools_for_tags(self) -> list: ...
-    
+    def get_tools_for_tags(self) -> list:
+        ...
+
     @abstractmethod
-    def create_tools(self) -> None: ...
-    
+    def create_tools(self) -> None:
+        ...
+
     @abstractmethod
-    def delete_tools(self) -> None: ...
+    def delete_tools(self) -> None:
+        ...
