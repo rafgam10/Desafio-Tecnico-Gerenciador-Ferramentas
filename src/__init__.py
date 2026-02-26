277143a7bcb8893209f73a7d1f2c8aa0034e45a9
Timoteo <timoteo@hdinfo-ma.com>
2026-02-26 15:05:53 -0300

    feat: Implementação de linhas para criar o DB.

--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from .settings.config import Config
 from .settings.extensions import db, migrate, jwt
 
+
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
@@ -10,9 +11,21 @@ def create_app():
     migrate.init_app(app, db)
     jwt.init_app(app)
 
+    from src.models.tools_model import Tool
+
     try:
+
+        with app.app_context():
+            db.create_all()
+
         from .routes import register_routes
+
         register_routes(app)
+
+        from src.routes.tools_routes import tools_bp
+
+        app.register_blueprint(tools_bp)
+
     except Exception:
         pass
 
