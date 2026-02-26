91675b35ffee0a8cebdb33ce0c14767f305eb40a
Timoteo <timoteo@hdinfo-ma.com>
2026-02-26 14:47:00 -0300

    feat: Correção da formatação do model tools..

--- src/models/tools_model.py
+++ src/models/tools_model.py
@@ -1,35 +1,36 @@
 from src.settings.extensions import db
 import json
 
+
 class Tool(db.Model):
-    
-    __tablename__ = "tools"
-    
+
+    __tablename__ = 'tools'
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(225), nullable=False)
     link = db.Column(db.String(2048), nullable=False)
     description = db.Column(db.Text, nullable=True)
     tags = db.Column(db.Text, nullable=False)
-    
+
     def __init__(self, title, link, description):
         self.title = title
         self.link = link
         self.description = description
-    
-    def set_tags(self, tags_list:list):
+
+    def set_tags(self, tags_list: list):
         self.tags = json.dumps(tags_list)
-        
+
     def get_tags(self) -> list:
         return json.loads(self.tags)
-    
+
     def to_dict(self):
         return {
-            "title": self.title,
-            "link": self.link,
-            "description": self.description,
-            "tags": self.get_tags(),
-            "id": self.id
+            'title': self.title,
+            'link': self.link,
+            'description': self.description,
+            'tags': self.get_tags(),
+            'id': self.id,
         }
-        
+
     def __repr__(self):
-        return f"ID: {self.id} - TITLE: {self.title}"
+        return f'ID: {self.id} - TITLE: {self.title}'
